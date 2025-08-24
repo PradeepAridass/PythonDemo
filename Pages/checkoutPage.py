720c758eec18a02955c13d5e16195e4dd4be02b5
PradeepAridass <pradeeparidass99@gmail.com>
2025-08-24 21:11:01 +0530

    New changes

--- Pages/checkoutPage.py
+++
@@ -1,8 +0,0 @@
-from Frame.conftest import driver
-from Utils.BrowserUtils import BrowserUtils
-
-
-class CheckoutPage(BrowserUtils):
-    def __init__(self):
-        super().__init__(driver)
-        self.driver= driver
